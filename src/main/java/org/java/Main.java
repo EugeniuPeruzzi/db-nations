1ca896dfd7a83f6707d9e4b4fc9f82f4707588ab
EugeniuPeruzzi <jaka1997@live.it>
2023-11-24 16:13:18 +0100

    milestone 3 done

--- src/main/java/org/java/Main.java
+++ src/main/java/org/java/Main.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class Main {
 	
@@ -13,15 +14,14 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		test1();
-
+		//milestone2();
+		milestone3();
 	}
 	
 	
 	
 	
-	private static void test1() {
-		
+	private static void milestone2() {
 		
 		try (Connection con = DriverManager.getConnection(url, user, pws)) {  
 		  
@@ -49,6 +49,47 @@ public class Main {
 			
 			System.out.println("Error in db: " + e.getMessage());
 		}
+		
 	}
+	
+	private static void milestone3() {
 
+		Scanner in = new Scanner(System.in);
+		System.out.println("Ricerca un paese");
+		
+		String userInput = in.nextLine();
+		
+		try (Connection con = DriverManager.getConnection(url, user, pws)) {  
+		  
+		  final String sql = " SELECT countries.country_id AS country_id , countries.name AS country_name, regions.name AS regions_name , continents.name AS continents_name "
+			  			   + " FROM countries "
+			  			   + " JOIN regions ON countries.region_id = regions.region_id "
+			  			   + " JOIN continents ON regions.continent_id = continents.continent_id "
+			  			   + " WHERE countries.name LIKE ? "
+			  			   + " ORDER BY countries.name"
+			  			   + " ; ";		  
+		  
+		  try(PreparedStatement ps = con.prepareStatement(sql)){
+			  ps.setString(1, "%" + userInput + "%");
+			  
+		    try(ResultSet rs = ps.executeQuery()){
+		    	
+		    	while(rs.next()) {
+		    		
+		    		int id = rs.getInt(1);
+		    		String nameCountry = rs.getString(2);
+		    		String nameRegion = rs.getString(3);
+		    		String nameContinent = rs.getString(4);
+		    		
+		    		System.out.println("[" + id + "] " + nameCountry + " - " + nameRegion + " - " + nameContinent + "\n");
+		    	}
+		    }
+		  }
+		} catch (Exception e) {
+			
+			System.out.println("Error in db: " + e.getMessage());
+		}
+		
+		in.close();
+	}
 }
